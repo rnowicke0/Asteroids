5f53eb5f129a07af11a190992ac20ba164c5e48b
rnowicke0 <rnowicke98@gmail.com>
2026-03-16 14:02:15 -0500

    add scoring system

--- asteroid.py
+++ asteroid.py
@@ -15,6 +15,7 @@ class Asteroid(CircleShape):
         self.position += self.velocity * dt
 
     def split(self):
+        score_count = 0
         self.kill()
         if self.radius <= ASTEROID_MIN_RADIUS:
             return
@@ -25,3 +26,4 @@ class Asteroid(CircleShape):
         new_asteroid2 = Asteroid(self.position[0], self.position[1], (new_radius - ASTEROID_MIN_RADIUS))
         new_asteroid1.velocity = pygame.math.Vector2.rotate(self.velocity, random_num) * 1.2
         new_asteroid2.velocity = pygame.math.Vector2.rotate(self.velocity, -random_num) * 1.2
+        score_count += 1
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT, SCORE_BOARD_WIDTH, SCORE_BOARD_HEIGHT
 from logger import log_state
 from player import Player
 from asteroid import Asteroid
@@ -18,9 +18,14 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     pygame.init()
+    pygame.font.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    # trying to add a scoreboard to keep track of player score
+    font = pygame.font.SysFont('Arial', 20)
+    text_box_rect = pygame.Rect(SCREEN_WIDTH / 2 - (SCORE_BOARD_WIDTH / 2), 0, SCORE_BOARD_WIDTH, SCORE_BOARD_HEIGHT)
+    score_board = pygame.Rect(SCREEN_WIDTH / 2 - (SCORE_BOARD_WIDTH / 2), 0, SCORE_BOARD_WIDTH, SCORE_BOARD_HEIGHT)
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
@@ -34,6 +39,8 @@ def main():
 
     asteroid_field = AsteroidField()
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    score_count = 0
 
     running = True
     while running:
@@ -51,6 +58,7 @@ def main():
             if asteroid.collides_with(player):
                 log_event("player_hit")
                 print(f"Game over!")
+                print(f"Your score was: {score_count}")
                 sys.exit()
 
         for asteroid in asteroids:
@@ -59,9 +67,18 @@ def main():
                     log_event("asteroid_shot")
                     shot.kill()
                     asteroid.split()
+                    score_count += 1
 
         screen.fill("black")
 
+        pygame.draw.rect(screen, "white", score_board)
+        pygame.draw.rect(screen, "black", text_box_rect, 2)
+
+        current_text = f"Score: {score_count}"
+        text_surface = font.render(current_text, True, "black")
+        text_rect = text_surface.get_rect(center=text_box_rect.center)
+        screen.blit(text_surface, text_rect)
+        
         for object in drawable:
             object.draw(screen)
         
